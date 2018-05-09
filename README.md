06e4ef097561b4423d2e2be1c0b79f40ce388773
sleeplesseditor <josh@bryson.me.uk>
2018-05-09 18:05:00 +0100

    SongList component setup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # LyricalGraphQL
 
 An experiment in the use of React with GraphQL, following directions from Stephen Grider's Udemy course.
+
+To get this up and running, follow these steps:
+
+1) cd LyricalGraphQL
+2) npm install
+3) In server.js, enter your own MLab URI
+4) npm run dev
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://joshgql:excelsior@ds117960.mlab.com:17960/lyricalgraphqldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
