dd7656fd07df0df3588f2da682972e902ac1983a
sleeplesseditor <josh@bryson.me.uk>
2018-05-17 00:09:42 +0100

    App finalised

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ To get this up and running, follow these steps:
 2) npm install
 3) In server.js, enter your own MLab URI
 4) npm run dev
+
+![screen shot 2018-05-17 at 00 04 35](https://user-images.githubusercontent.com/25869284/40148520-4dbddbce-5966-11e8-84be-8c507f87562b.png)
+
+App landing page with songs and 'add song' button
+
+
+![screen shot 2018-05-17 at 00 04 50](https://user-images.githubusercontent.com/25869284/40148540-651535d8-5966-11e8-83f0-97b07ae77dec.png)
+
+Create song page
+
+
+![screen shot 2018-05-17 at 00 05 00](https://user-images.githubusercontent.com/25869284/40148555-754465e6-5966-11e8-9e87-9655b7e644d2.png)
+
+Song with add lyric form
+
+
+![screen shot 2018-05-17 at 00 06 27](https://user-images.githubusercontent.com/25869284/40148570-85614d04-5966-11e8-9a93-eeb2a10116ca.png)
+
+Song with displayed lyrics and like feature
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { optimistic } from 'apollo-client/optimistic-data/store';
 
 class LyricList extends Component {
-    onLike(id) {
-        this.props.mutate({ variables: { id }})
+    onLike(id, likes) {
+        this.props.mutate({ 
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        });
     }
 
     renderLyrics() {
@@ -12,13 +23,15 @@ class LyricList extends Component {
             return (
                 <li key={id} className="collection-item">
                     {content}
-                    <i 
-                        className="material-icons"
-                        onClick={() => this.onLike(id)}
-                    >
-                        thumb_up
-                    </i>
-                    {likes}
+                    <div className="vote-box">
+                        <i 
+                            className="material-icons"
+                            onClick={() => this.onLike(id, likes)}
+                        >
+                            thumb_up
+                        </i>
+                        {likes}
+                    </div>
                 </li>
             );
         });
