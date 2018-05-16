7c80652255d1c2d3ac14d4f9c13af6ae1855d885
sleeplesseditor <josh@bryson.me.uk>
2018-05-16 23:44:59 +0100

    Like feature added

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+    onLike(id) {
+        this.props.mutate({ variables: { id }})
+    }
+
     renderLyrics() {
-        return this.props.lyrics.map(({ id, content }) => {
+        return this.props.lyrics.map(({ id, content, likes }) => {
             return (
                 <li key={id} className="collection-item">
                     {content}
+                    <i 
+                        className="material-icons"
+                        onClick={() => this.onLike(id)}
+                    >
+                        thumb_up
+                    </i>
+                    {likes}
                 </li>
             );
         });
@@ -21,4 +34,13 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LikeLyric($id: ID) {
+        likeLyric(id: $id) {
+            id
+            likes
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricList);
