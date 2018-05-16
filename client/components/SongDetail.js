50e3790c3d02ee3affed2ef4b1ca532239b46b36
sleeplesseditor <josh@bryson.me.uk>
2018-05-16 12:45:08 +0100

    LyricCreate and LyricList component setup

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
+import { Link } from 'react-router';
+
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
     render() {
+        const { song } = this.props.data;
+
+        if (!song) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
-                <h3>Song Detail</h3>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
+                <LyricList />
+                <LyricCreate songId={this.props.params.id}/>
             </div>
         );
     }
